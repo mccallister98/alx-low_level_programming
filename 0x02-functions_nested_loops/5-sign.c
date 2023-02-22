751c238476d94b86a0a39a330f7efeb20f084192
mccallister98 <machatiwandi@outlook.com>
2023-02-22 17:05:00 +0200

    Functions, nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - checks if n is +/-/0
+ * @n: character being checked
+ * Return: 1 if n is positive/ 0 if n is zero/ -1 if n is negative
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar(43);
+		return (1);
+	}
+
+	else if (n < 0)
+	{
+		_putchar(45);
+		return (-1);
+	}
+
+	else
+	{
+		_putchar(48);
+		return (0);
+	}
+}
